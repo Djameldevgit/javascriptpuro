e8bf6c3a59a0a7e18cf429c4c1f815473eeb5cbe
Djameldevgit <developpementwebdjamel@gmail.com>
2025-02-03 10:04:48 +0000

    jjjjj

--- server.js
+++ server.js
@@ -1,19 +1,61 @@
-const express = require('express');
-const path = require('path');
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const SocketServer = require('./socketServer')
+ 
+const path = require('path')
 
-const app = express();
 
-// Middleware para servir archivos estáticos
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+
+
+// Socket
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+
+io.on('connection', socket => {
+    SocketServer(socket)
+})
+
+ 
+
+// Routes
+app.use('/api', require('./routes/authRouter'))
+app.use('/api', require('./routes/userRouter'))
+app.use('/api', require('./routes/postRouter'))
+app.use('/api', require('./routes/commentRouter'))
+app.use('/api', require('./routes/notifyRouter'))
+app.use('/api', require('./routes/messageRouter'))
+
+
+const URI = process.env.MONGODB_URL
+mongoose.connect(URI, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, err => {
+    if(err) throw err;
+    console.log('Connected to mongodb')
+})
+
+ 
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Ruta para manejar cualquier otra solicitud y devolver el index.html (importante para SPA)
-app.get('*', (req, res) => {
+// Ruta para el archivo principal
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Usar el puerto proporcionado por Render o el 3000 por defecto
+// En producción, usamos el puerto que nos da el entorno, o un puerto local por defecto
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
-
